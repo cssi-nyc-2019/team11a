3dc90cf5eb406eb4a726f3862dbe78a859b933d5
XxUnknown <43911590+XxUnknown@users.noreply.github.com>
2019-07-25 14:49:00 -0400

    Merging

--- main.py
+++ main.py
@@ -26,22 +26,7 @@ class Login(webapp2.RequestHandler):
 		login_template  = the_jinja_env.get_template('templates/login.html')
 		self.response.write(login_template.render())
 
-	def post(self):
-		dash_template =  the_jinja_env.get_template('templates/dashboard.html')
-		logg_template =  the_jinja_env.get_template('templates/login.html')
-		username  = self.request.get('username')
-		password  = self.request.get('password')
-		query=User.query().fetch()
-		loggedIn=False
-
-		for element  in  query:
-			if (element.username  ==  username) and  (element.password == password):
-				self.response.write(dash_template.render())
-				loggedIn=True
-				break
-			 
-		if  loggedIn==False:
-			self.response.write(logg_template.render())
+	
 		
 
 class Signup(webapp2.RequestHandler):
@@ -66,6 +51,26 @@ class Dashboard(webapp2.RequestHandler):
 		}
 		dash_template = the_jinja_env.get_template('templates/dashboard.html')
 		self.response.write(dash_template.render(dash_dict))
+	def post(self):
+		dash_dict = {
+		'date': str(datetime.date.today().strftime("%d"))+" "+str(datetime.date.today().strftime("%B"))+" "+str(datetime.date.today().strftime("%Y"))
+		}
+
+		dash_template =  the_jinja_env.get_template('templates/dashboard.html')
+		logg_template =  the_jinja_env.get_template('templates/login.html')
+		username  = self.request.get('username')
+		password  = self.request.get('password')
+		query=User.query().fetch()
+		loggedIn=False
+
+		for element  in  query:
+			if (element.username  ==  username) and  (element.password == password):
+				self.response.write(dash_template.render(dash_dict))
+				loggedIn=True
+				break
+			 
+		if  loggedIn==False:
+			self.response.write(logg_template.render())
 
 
 
